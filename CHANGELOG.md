835129a86eea5f870891af4be787ea3a861b41f9
Pratyush Mittal <pratyushmittal@gmail.com>
2022-09-22 18:01:21 +0530

    Added auto-reuse of auth token

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 The changelog was added version 0.3.0 onwards.
 
+## v0.5.0
+
+Added automatic re-use of auth-token. The auth-token generated at IRP (Invoice Registration Portal) is valid for 6 hours. The portal recommends re-using the same token till expiry. This is automatically done by the library now.
+
 ## v0.4.0
 
 Added `session.get` and `session.post` methods. This allows us to use the API with any endpoint.
--- src/gst_irn/session.py
+++ src/gst_irn/session.py
@@ -1,7 +1,10 @@
 import base64
+import datetime as dt
 import json
 import logging
 import os
+from hashlib import md5
+from pathlib import Path
 from pprint import pformat
 
 import requests
@@ -57,6 +60,92 @@ def _get_data_from_response(response, *, encryption_key):
         _raise_formatted_error(response.text, f"status {response.status_code}")
 
 
+def _get_auth_token(session, force_regenerate_token):
+    app_key = _get_app_key()
+    url = f"{session.base_url}/eivital/v1.04/auth"
+    # request headers
+    headers = {
+        "client-id": session.client_id,
+        "client-secret": session.client_secret,
+        "gstin": session.gstin,
+    }
+    if session.gsp_headers:
+        headers.update(session.gsp_headers)
+
+    # request payload
+    payload = {
+        "UserName": session.username,
+        "Password": session.password,
+        "AppKey": app_key,
+        "ForceRefreshAccessToken": force_regenerate_token,
+    }
+    # convert payload to json string
+    payload = json.dumps(payload)
+
+    # base64 encoding of payload
+    payload = base64.b64encode(payload.encode())
+
+    # encrypt using e-Invoice public Key
+    payload = crypto.encrypt_with_rsa_pub_key(payload, session.public_key)
+
+    payload = {"Data": payload}
+    response = requests.post(url, json=payload, headers=headers)
+
+    data = _get_data_from_response(response, encryption_key=None)
+    return data, app_key
+
+
+def _get_store_path(cache_dir, session):
+    # prepare dictionary of all session variables
+    attr_dict = {
+        a: getattr(session, a)
+        for a in dir(session)
+        if not a.startswith("_") and not callable(getattr(session, a))
+    }
+
+    # create a hash for session values
+    repr = json.dumps(attr_dict, sort_keys=True)
+    cache_key = md5(repr.encode()).hexdigest()
+
+    filename = f"{cache_key}.json"
+    store_path = Path(cache_dir) / filename
+    return store_path
+
+
+def _get_stored_token(cache_dir, session):
+    store_path = _get_store_path(cache_dir, session)
+    try:
+        stored_token = json.loads(store_path.read_text())
+
+        # check expiry
+        expiry = dt.datetime.strptime(
+            stored_token["expiry_time"], "%Y-%m-%d %H:%M:%S"
+        )
+        if expiry < (dt.datetime.now() + dt.timedelta(minutes=10)):
+            stored_token = None
+
+        # return tuple of token and sek
+        else:
+            stored_token = (stored_token["token"], stored_token["sek"])
+    except Exception:
+        stored_token = None
+    return stored_token
+
+
+def _store_token(cache_dir, session, expiry_time):
+    store_path = _get_store_path(cache_dir, session)
+    try:
+        store_path.parent.mkdir(parents=True, exist_ok=True)
+        obj = {
+            "token": session._auth_token,
+            "sek": session._auth_sek,
+            "expiry_time": expiry_time,
+        }
+        store_path.write_text(json.dumps(obj))
+    except Exception:
+        pass
+
+
 class Session:
     def __init__(
         self,
@@ -82,40 +171,20 @@ class Session:
         self._auth_token = None
         self._auth_sek = None
 
-    def generate_token(self, force_regenerate_token=False):
-        app_key = _get_app_key()
-        url = f"{self.base_url}/eivital/v1.04/auth"
-        # request headers
-        headers = {
-            "client-id": self.client_id,
-            "client-secret": self.client_secret,
-            "gstin": self.gstin,
-        }
-        if self.gsp_headers:
-            headers.update(self.gsp_headers)
+    def generate_token(
+        self, force_regenerate_token=False, cache_dir="tokens_cache"
+    ):
+        stored_token = _get_stored_token(cache_dir, self)
+        if stored_token and not force_regenerate_token:
+            self._auth_token, self._auth_sek = stored_token
+        else:
+            data, app_key = _get_auth_token(self, force_regenerate_token)
+            self._auth_token = data["AuthToken"]
+            self._auth_sek = _get_decrypted_sek(data["Sek"], app_key)
 
-        # request payload
-        payload = {
-            "UserName": self.username,
-            "Password": self.password,
-            "AppKey": app_key,
-            "ForceRefreshAccessToken": force_regenerate_token,
-        }
-        # convert payload to json string
-        payload = json.dumps(payload)
-
-        # base64 encoding of payload
-        payload = base64.b64encode(payload.encode())
-
-        # encrypt using e-Invoice public Key
-        payload = crypto.encrypt_with_rsa_pub_key(payload, self.public_key)
-
-        payload = {"Data": payload}
-        response = requests.post(url, json=payload, headers=headers)
-
-        data = _get_data_from_response(response, encryption_key=None)
-        self._auth_token = data["AuthToken"]
-        self._auth_sek = _get_decrypted_sek(data["Sek"], app_key)
+            # cache token
+            expiry_time = data["TokenExpiry"]
+            _store_token(cache_dir, self, expiry_time)
 
     def _get_request_headers(self):
         headers = {
